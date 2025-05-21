67d30e77e8e7660aae135426ea0df31dfe32c4c6
Matheca123 <mateuscorreia14@gmail.com>
2025-05-20 21:21:29 -0300

    Update InstrutorRepository.java

--- SmartAcademy/src/main/java/com/example/SmartAcademy/repositories/InstrutorRepository.java
+++ SmartAcademy/src/main/java/com/example/SmartAcademy/repositories/InstrutorRepository.java
@@ -1,12 +1,13 @@
 package com.example.SmartAcademy.repositories;
 
 import com.example.SmartAcademy.models.InstrutorModels;
+
 import java.util.List;
 
 public interface InstrutorRepository {
+    InstrutorModels buscarPorCodigo(int codigo);
+    List<InstrutorModels> buscar();
     void adicionar(InstrutorModels instrutor);
     void atualizar(InstrutorModels instrutor);
     void remover(int codigo);
-    List<InstrutorModels> buscar();
-    InstrutorModels buscarPorCodigo(int codigo);
 }
