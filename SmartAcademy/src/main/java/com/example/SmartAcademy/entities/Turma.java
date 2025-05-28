2e53c1a800213b53c85960e73a9d4229c396ca9f
Matheca123 <mateuscorreia14@gmail.com>
2025-05-28 20:36:26 -0300

    Update Turma.java

--- SmartAcademy/src/main/java/com/example/SmartAcademy/entities/Turma.java
+++ SmartAcademy/src/main/java/com/example/SmartAcademy/entities/Turma.java
@@ -1,4 +1,27 @@
 package com.example.SmartAcademy.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "turmas")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
 public class Turma {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_turma")
+    private Long id;
+
+    @Column(name = "horario_time")
+    private LocalTime horario;
+
+    @ManyToOne
+    @JoinColumn(name = "id_atividade")
+    private Atividade atividade;
+
+    @ManyToOne
+    @JoinColumn(name = "Sala_id_sala")
+    private Sala sala;
 }
