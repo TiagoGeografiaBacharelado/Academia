521f80c358e4261f99e062c2b8c72b7f9ec09f4f
Matheca123 <mateuscorreia14@gmail.com>
2025-05-28 20:31:48 -0300

    Update TurmaJPA.java

--- SmartAcademy/src/main/java/com/example/SmartAcademy/repositories/jpa/TurmaJPA.java
+++ SmartAcademy/src/main/java/com/example/SmartAcademy/repositories/jpa/TurmaJPA.java
@@ -1,7 +1,9 @@
 package com.example.SmartAcademy.repositories.jpa;
 
-import com.example.SmartAcademy.models.TurmaModels;
+import com.example.SmartAcademy.entities.Turma;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface TurmaJPA extends JpaRepository<TurmaModels, Integer> {
+@Repository
+public interface TurmaJPA extends JpaRepository<Turma, Long> {
 }
