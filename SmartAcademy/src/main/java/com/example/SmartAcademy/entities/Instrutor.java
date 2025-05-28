f1bbe95882d71fcf8bb127c3feb0ebf07356a129
Matheca123 <mateuscorreia14@gmail.com>
2025-05-28 20:32:58 -0300

    Update Instrutor.java

--- SmartAcademy/src/main/java/com/example/SmartAcademy/entities/Instrutor.java
+++ SmartAcademy/src/main/java/com/example/SmartAcademy/entities/Instrutor.java
@@ -1,4 +1,37 @@
 package com.example.SmartAcademy.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "instrutor")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
 public class Instrutor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_instrutor")
+    private Long id;
+
+    @Column(length = 100)
+    private String nome;
+
+    @Column(length = 45)
+    private String cpf;
+
+    @Column(length = 45)
+    private String certificado;
+
+    @Column(length = 100)
+    private String email;
+
+    @Column(length = 45)
+    private String telefone;
+
+    @Column(name = "data_nascimento")
+    private LocalDate dataNascimento;
+
+    @Column(length = 100)
+    private String endereco;
 }
