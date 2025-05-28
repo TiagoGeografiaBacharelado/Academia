0a2ee4db2c44abcb0759a0e0c199e54071a12f16
Matheca123 <mateuscorreia14@gmail.com>
2025-05-28 20:33:22 -0300

    Update Sala.java

--- SmartAcademy/src/main/java/com/example/SmartAcademy/entities/Sala.java
+++ SmartAcademy/src/main/java/com/example/SmartAcademy/entities/Sala.java
@@ -1,4 +1,19 @@
 package com.example.SmartAcademy.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "sala")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
 public class Sala {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_sala")
+    private Long id;
+
+    @Column(length = 50)
+    private String nome;
+
+    private Integer capacidade;
 }
