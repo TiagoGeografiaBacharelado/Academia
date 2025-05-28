5e3e7adf249b05916aa06c9c67e6bed50ff4e71c
Matheca123 <mateuscorreia14@gmail.com>
2025-05-28 20:33:09 -0300

    Update InstrutorTurma.java

--- SmartAcademy/src/main/java/com/example/SmartAcademy/entities/InstrutorTurma.java
+++ SmartAcademy/src/main/java/com/example/SmartAcademy/entities/InstrutorTurma.java
@@ -1,4 +1,25 @@
 package com.example.SmartAcademy.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "instrutorturma")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
 public class InstrutorTurma {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_instrutor", nullable = false)
+    private Instrutor instrutor;
+
+    @ManyToOne
+    @JoinColumn(name = "id_turma", nullable = false)
+    private Turma turma;
+
+    @Column(name = "Cliente_id_cliente")
+    private Long clienteId;
 }
